6be864a6323adc046710bded049b5e36c2f9195a
zeidalidiez <zeidalidiez@gmail.com>
2021-01-25 00:29:03 -0800

    setup

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export default {
-  API_ENDPOINT: 'https://desolate-dawn-86670.herokuapp.com/api',
-  // API_ENDPOINT : 'http://localhost:8000/api',
+  // API_ENDPOINT: 'https://desolate-dawn-86670.herokuapp.com/api',
+  API_ENDPOINT : 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 };
